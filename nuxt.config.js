aaf27df89085f95fb9736f3e8efadab9d1a9e1ac
dosiss <alexander.kharin@gmail.com>
2023-05-04 15:57:50 +0300

    gtm script fix

--- nuxt.config.js
+++ nuxt.config.js
@@ -64,6 +64,7 @@ export default {
       }
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    __dangerouslyDisableSanitizers: ['script'],
     script: [
   {
     id: 'hs-script-loader',
@@ -73,7 +74,7 @@ export default {
   },
   {
     hid: 'gtm-script',
-    innerHtml: `
+    innerHTML: `
     (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
