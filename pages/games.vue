562e829b53be1c120005a65c3657b2db6f0fed94
Alexander Kharin <alexander.kharin@gmail.com>
2026-02-26 11:24:43 +0200

    vertical slider add

--- pages/games.vue
+++ pages/games.vue
@@ -68,7 +68,7 @@
                   <v-lazy-image :src="`/images/${game.thumbUrl}`" :src-placeholder="`/images/lowres/${game.thumbUrl.substring(0, game.thumbUrl.lastIndexOf('.'))}.jpeg`" :alt="`${game.name}`" class="game-thumbnail__img" loading="lazy" />
                   <div class="game-content__wrap">
                     <div class="game-content__buttns aa">
-                      <nuxt-link :to="`/${game.slug}`" class="buttn buttn-secondary buttn-sm">{{ $device.isMobile ? $t('More') : $t('Learn more') }}</nuxt-link>
+                      <nuxt-link :to="localeLocation( {path: `/${game.slug}`} )" class="buttn buttn-secondary buttn-sm">{{ $device.isMobile ? $t('More') : $t('Learn more') }}</nuxt-link>
                       <!-- <NuxtLink :to="game.slug" class="buttn buttn-secondary buttn-sm">{{ $device.isMobile ? $t('More') : $t('Learn more') }}</NuxtLink> -->
                       <div v-if="game.comingSoon === true" class="buttn buttn-secondary buttn-disabled buttn-sm">{{ $t('Coming soon') }}</div>
                       <div v-else>
@@ -105,7 +105,7 @@
                   <v-lazy-image :src="`/images/${game.thumbUrl}`" :src-placeholder="`/images/lowres/${game.thumbUrl.substring(0, game.thumbUrl.lastIndexOf('.'))}.jpeg`" :alt="`${game.name}`" class="game-thumbnail__img" loading="lazy" />
                   <div class="game-content__wrap">
                     <div class="game-content__buttns bb">
-                      <nuxt-link :to="`/${game.slug}`" class="buttn buttn-secondary buttn-sm">{{ $device.isMobile ? $t('More') : $t('Learn more') }}</nuxt-link>
+                      <nuxt-link :to="localeLocation( {path: `/${game.slug}`} )" class="buttn buttn-secondary buttn-sm">{{ $device.isMobile ? $t('More') : $t('Learn more') }}</nuxt-link>
                       <div v-if="game.comingSoon === true" class="buttn buttn-secondary buttn-disabled buttn-sm">{{ $t('Coming soon') }}</div>
                       <div v-else>
                         <a v-if="$device.isMobile" :href="`https://${game.linkToDemo}`" class="buttn buttn-colored buttn-m buttn-icon">
